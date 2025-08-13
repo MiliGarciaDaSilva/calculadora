bb2a0c3c0953aecbec1e477bf651ced65018f6c1
Hernan Lopez <hlhernano@gmail.com>
2025-08-13 18:23:19 -0300

    Puse la suma y la division en el archivo Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,3 +2,21 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+
+// Esta clase implementa la operación Dividir
+public class Division
+{
+    public static double Divide(int a, int b)
+    {
+        return (double)a / b;
+    }
+}
+
+// Esta clase implementa la operación Sumar
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
